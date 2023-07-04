b5a6068aad4b734459af93c2c58b3fc1abe44406
kaushalgabani <kaushalgabani21@gmail.com>
2023-07-03 22:58:40 -0400

    Generate and added shopping cart feature

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 import { GetResponseProducts } from 'src/app/services/product.service';
 
@@ -26,7 +28,8 @@ export class ProductListComponent {
 
 
   constructor(private productService: ProductService,
-    private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private cartService: CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
@@ -91,6 +94,15 @@ export class ProductListComponent {
     if (theKeyword) {
       this.productService.searchProductPaginate(this.thePageNumber - 1, this.thePageSize, theKeyword).subscribe(this.processResult);
     }
+  }
+
+  // a method that adds item to cart
+  addToCart(theProduct: Product){
+    console.log(`Adding item into cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
   }
 
   processResult = (data: GetResponseProducts) => {
---
+++ angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>;
+  totalQuantity: Subject<number> = new Subject<number>;
+
+  constructor() { }
+
+  addToCart(theCartItem: CartItem) {
+    //check if we already have item in the cart
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem | null = null;
+
+    if (this.cartItems.length > 0) {
+      //find the item in the cart based on item id
+      for (let tmpCartItem of this.cartItems) {
+        if (tmpCartItem.id === theCartItem.id) {
+          existingCartItem = tmpCartItem;
+          break;
+        }
+      }
+    }
+
+    //check if we found it
+    alreadyExistsInCart = (existingCartItem != undefined);
+
+    if (existingCartItem != null) {
+      //increment the quantity 
+      existingCartItem.quantity++;
+    } else {
+      //add item to the array
+      this.cartItems.push(theCartItem);
+    }
+
+    // calculate cart total price and total quantity
+    this.computeCartTotals();
+  }
+
+  computeCartTotals(){
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for(let tmpCartItem of this.cartItems){
+      totalPriceValue += tmpCartItem.quantity * tmpCartItem.unitPrice;
+      totalQuantityValue += tmpCartItem.quantity;
+    }
+
+    // publish the new values, all the subscribers will receive new data
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    // log cart data 
+    this.logCartData(totalPriceValue, totalQuantityValue);
+  }
+
+  logCartData(totalPriceValue: number, totalQuantityValue:number){
+    console.log('Contents of the cart');
+
+    for(let tmpCartItem of this.cartItems){
+      const subTotalPrice = tmpCartItem.quantity * tmpCartItem.unitPrice;
+      console.log(`Item name: ${tmpCartItem.name}, Quantity: ${tmpCartItem.quantity}, Unit Price: ${tmpCartItem.unitPrice}, Sub Total Price: ${subTotalPrice}`);
+    }
+
+    console.log(`Total Price: ${totalPriceValue.toFixed(2)}, Total Quantity: ${totalQuantityValue}`);
+    console.log(`-----------------------------`);
+  }
+}
